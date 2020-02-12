8cdb9a3c341b71b00abd1182eeed90c8b1013b1b
최성익 <tjddlr92@gmail.com>
2020-02-12 18:58:29 +0900

    Refactor: JS / ES 클래스 형식으로 변경
    기존의 코드를 클래스를 나눠 구현했다. 이미지와 텍스트가 들어있는 Carousel 클래스, 이전버튼, 다음버튼이 들어있는 Button 클래스. (Card는 미구현)
    캐러셀에서는 스스로 움직이는 기능만 구현하고, 버튼이 클릭됐다는 카운트를 넘겨주는 방식으로 구현했다. 맞는 방식인지는 모르겠다.
    일종의 의인화를 한건데,  너무 클래스가 많아지는 것 같기도 하다.

--- slider.js
+++ slider.js
@@ -1,18 +1,51 @@
-window.addEventListener("DOMContentLoaded", event => {
-  const carousel = document.querySelector(".slider__list");
-  const arrayLI = document.querySelectorAll(".slider__item");
-  const imageSize = document.querySelector(".slider__item").offsetWidth;
-  const previousBtn = document.querySelector(".btn__previous");
-  const nextBtn = document.querySelector(".btn__next");
-  let count = 0;
+class Carousel {
+  constructor(elem) {
+    this.elem = document.querySelector(elem);
+    this.offset = this.elem.firstElementChild.offsetWidth;
+    this.size = this.elem.childElementCount - 1;
+    this.index = 0;
+  }
 
-  nextBtn.addEventListener("click", event => {
-    count--;
-    carousel.style.transform = `translateX(${count * imageSize}px)`;
+  move(count) {
+    this.index += count;
+    this.checkSize(this.index);
+    this.elem.style.transform = `translateX(-${this.index * this.offset}px)`;
+  }
+
+  checkSize(index) {
+    if (index < 0) this.index = this.size;
+    else if (index > this.size) this.index = 0;
+  }
+}
+
+class Button {
+  constructor(prevBtn, nextBtn) {
+    this.prevBtn = document.querySelector(prevBtn);
+    this.nextBtn = document.querySelector(nextBtn);
+  }
+
+  clickPrev() {
+    return -1;
+  }
+
+  clickNext() {
+    return 1;
+  }
+}
+
+class Card {
+  constructor() {}
+}
+
+window.addEventListener("DOMContentLoaded", event => {
+  const carousel = new Carousel(".slider__list");
+  const button = new Button(".btn__previous", ".btn__next");
+
+  button.prevBtn.addEventListener("click", e => {
+    carousel.move(button.clickPrev());
   });
 
-  previousBtn.addEventListener("click", event => {
-    count++;
-    carousel.style.transform = `translateX(${count * imageSize}px)`;
+  button.nextBtn.addEventListener("click", e => {
+    carousel.move(button.clickNext());
   });
 });
