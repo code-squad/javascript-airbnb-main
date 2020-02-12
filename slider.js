9d5f9d1d751209b3184b2415b2508d2ca0cffd81
최성익 <tjddlr92@gmail.com>
2020-02-12 22:06:28 +0900

    Refactor: Carousel 단일 클래스로 합치기
    Button, Card 클래스가 하는 일이 너무 적은 거 같아, 캐러셀 클래스 하나로 통합했다. (clickcard(), clickbutton())
    문법적으로 이상한 부분들을 수정하고, 클래스 생성시 생성자의 인자를 객체로 활용하는 법을 이제 이해하고 사용해봤다.
    변수명을 이해하기 쉽게 다듬었다.
    HTML CSS에서 불필요한 부분을 삭제했다.

--- slider.js
+++ slider.js
@@ -1,57 +1,54 @@
 class Carousel {
-  constructor(elem) {
-    this.elem = document.querySelector(elem);
-    this.offset = this.elem.firstElementChild.offsetWidth;
-    this.size = this.elem.childElementCount - 1;
+  constructor(option) {
+    this.item = document.querySelector(option.item);
+    this.offset = this.item.firstElementChild.offsetWidth;
+    this.size = this.item.childElementCount - 1;
     this.index = 0;
+    this.button = document.querySelector(option.button);
+    [this.button.prev, this.button.next] = this.button.children;
+    this.card = document.querySelectorAll(option.card);
+    this.clickButton();
+    this.clickCard();
   }
 
-  move(count) {
-    this.index += count;
-    this.checkSize(this.index);
-    this.elem.style.transform = `translateX(-${this.index * this.offset}px)`;
+  slideItem() {
+    this.item.style.transform = `translateX(-${this.index * this.offset}px)`;
   }
 
-  movetoIndex(count) {
-    this.index = count;
-    this.elem.style.transform = `translateX(-${this.index * this.offset}px)`;
-  }
-
-  checkSize(index) {
-    if (index < 0) this.index = this.size;
-    else if (index > this.size) this.index = 0;
-  }
-}
-
-class Button {
-  constructor(elem) {
-    this.elem = document.querySelector(elem);
-  }
-
-  click(target) {
-    if (target.id === "prev") return -1;
-    else if (target.id === "next") return 1;
-  }
-}
-
-class Card {
-  constructor(elem) {
-    this.elem = document.querySelectorAll(elem);
-  }
-}
-
-window.addEventListener("DOMContentLoaded", event => {
-  const carousel = new Carousel(".slider__list");
-  const button = new Button(".slider__btn");
-  const card = new Card(".card-category__card");
-
-  button.elem.addEventListener("click", e => {
-    carousel.move(button.click(e.target));
-  });
-
-  card.elem.forEach((target, index) => {
-    card.elem[index].addEventListener("click", () => {
-      carousel.movetoIndex(index);
+  clickButton() {
+    this.button.prev.addEventListener("click", () => {
+      this.index -= 1;
     });
+
+    this.button.next.addEventListener("click", () => {
+      this.index += 1;
+    });
+
+    this.button.addEventListener("click", () => {
+      this.checkSize(this.index);
+      this.slideItem();
+    });
+  }
+
+  clickCard() {
+    this.card.forEach((eachCard, index) => {
+      eachCard.addEventListener("click", () => {
+        this.index = index;
+        this.slideItem();
+      });
+    });
+  }
+
+  checkSize(currentIndex) {
+    if (currentIndex < 0) this.index = this.size;
+    else if (currentIndex > this.size) this.index = 0;
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const carousel = new Carousel({
+    item: ".slider__list",
+    button: ".slider__btn",
+    card: ".card-category__card",
   });
 });
